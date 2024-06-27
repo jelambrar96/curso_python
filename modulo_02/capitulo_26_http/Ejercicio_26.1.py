4adfac40ee008beb3babfa2d48253370f73dae6e
Jorge Lambrano <jelambrar@gmail.com>
2024-06-27 00:27:07 -0500

    capitulo_26

---
+++ modulo_02/capitulo_26_http/Ejercicio_26.1.py
@@ -0,0 +1,9 @@
+"""
+### Ejercicio 1: Realizar una solicitud GET y extraer datos
+
+**Objetivo:** Realizar una solicitud GET a una API pública y 
+extraer información específica de la respuesta JSON.
+
+URL = 'http://github.com/jelambrar96/curso_python'
+
+"""
