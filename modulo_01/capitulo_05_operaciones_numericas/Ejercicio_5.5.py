5f5837c124c807437a55a0f56fc297d06e667547
Jorge Lambrano <jelambrar@gmail.com>
2024-08-05 09:44:02 -0500

    ejercicios_capitulo_5

---
+++ modulo_01/capitulo_05_operaciones_numericas/Ejercicio_5.5.py
@@ -0,0 +1,8 @@
+## Ejercicio 5.5
+
+# Construye una calculadora para hallar el area de un cilindro
+# 1. usa la funcion input para preguntar los valores de la altura
+# el radio
+# 2. calcula el area 
+# 3. imprime el resultado
+
---
+++ modulo_01/capitulo_05_operaciones_numericas/Ejercicio_5.6.py
@@ -0,0 +1,7 @@
+# Ejercicio 5.6
+
+# Calcular el valor de la hipotenusa
+# 1. usa la funcion input para preguntar por la longitud de los catetos x y y
+# 2. calcula el valor de la hipotenusa
+# 3. calcula el valor del angulo entre la hipotenusa y el cateto x
+# 4. calcula las funciones trigonométricas seno, coseno y tangente de ese ángulo. 
