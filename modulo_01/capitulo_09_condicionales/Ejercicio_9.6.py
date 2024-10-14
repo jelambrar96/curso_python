a6c4716b9084deeb1f6ff9a323a855cc912c5980
jelambrar96 <jelambrar@gmail.com>
2024-10-13 19:15:39 -0500

    ejercicios_cap_9

---
+++ modulo_01/capitulo_09_condicionales/Ejercicio_9.6.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 9.6
+
+Escribe un programa que utilice un condicional if-else para verificar si un número
+ingresado por el usuario es múltiplo de otro número ingresado por el usuario.
+
+El programa debe mostrar un mensaje indicando si el número es múltiplo o no.
+"""
---
+++ modulo_01/capitulo_09_condicionales/Ejercicio_9.9.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 9.9
+
+Escribe un programa que utilice un condicional if-else para verificar
+si una cadena contiene una subcadena ingresada por el usuario.
+
+El programa debe mostrar un mensaje indicando si la cadena contiene
+la subcadena o no.
+"""
